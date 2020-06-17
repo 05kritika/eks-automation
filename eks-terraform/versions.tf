7fa5de664ad78325b962aec6e8818879a33a4f8f
05kritika <39408273+05kritika@users.noreply.github.com>
2020-06-17 22:43:20 +0530

    Create versions.tf

---
+++ eks-terraform/versions.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "kubernetes" {	
+  load_config_file       = "false"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
