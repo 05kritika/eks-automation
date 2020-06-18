cd763961beb037e0119db31b19a17c4b53ce8f25
05kritika <39408273+05kritika@users.noreply.github.com>
2020-06-18 22:01:19 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
  # required_version = "~> 0.10"
   backend "s3"{
-    bucket                 = "kritika-terraform"
+    bucket                 = "eks-coe"
     region                 = "us-east-1"
     key                    = "backup.tf"
 #    workspace_key_prefix   = ""
