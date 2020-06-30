c14b01bf25ad6c409c25cd6b100d56f3ef401d1e
05kritika <39408273+05kritika@users.noreply.github.com>
2020-06-30 19:38:37 +0530

    Update runterraform.js.py

--- eks-terraform/runterraform.js.py
+++ eks-terraform/runterraform.js.py
@@ -3,7 +3,7 @@ import sys
 from subprocess import call
 from subprocess import check_output
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     print "Error wrong parameter number"
     print "Usage: python runterraform.py region prefix destroy"
     print "Example: python runterraform.py us-east-1 peppetest1 False"
@@ -11,15 +11,20 @@ if len(sys.argv) != 4:
 
 myregion = sys.argv[1]
 prefix = sys.argv[2]
-destroy = sys.argv[3]
+apply = sys.argv[3]
+destroy = sys.argv[4]
 
 mycommands = ["terraform"]
 
 if destroy == "True":
     mycommands.extend(["destroy","--force"])
   #  exit
-else:
+else 
     mycommands.extend(["apply","-auto-approve"])
+#elif apply=="True":
+#    mycommands.extend(["apply","-auto-approve"])
+#else
+#    exit
 
 mycommands.extend([ "-var", "region="+myregion , "-var", "prefix="+prefix])
 #mycommands.extend([ "-var", "region="+myregion])
