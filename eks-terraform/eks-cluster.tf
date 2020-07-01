20f812ca2886c29d43e368376b6ec36b766b7509
05kritika <39408273+05kritika@users.noreply.github.com>
2020-07-01 12:11:55 +0530

    Update eks-cluster.tf

--- eks-terraform/eks-cluster.tf
+++ eks-terraform/eks-cluster.tf
@@ -14,9 +14,9 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-node-1"
-      instance_type                 = "t2.small"
+      instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
@@ -24,7 +24,7 @@ module "eks" {
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = 2
     },
   ]
 }
